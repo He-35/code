d4c105693370a63cade1567f3c09027b717e66b0
Xiaohe35 <h13516092609@qq.com>
2025-09-25 17:22:48 +0800

    work

---
+++ 111.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <conio.h>
+int main()
+{
+for(float y=1.5f;y>-1.5f;y-=0.1f)
+{
+for(float x=-1.5f;x<1.5f;x+=0.05f)
+{
+float a=x*x+y*y-1;
+putchar(a*a*a-x*x*y*y*
+y<=0.0f?'&':' ');
+   }
+   putchar('\n');
+ }
+ for(;!kbhit();)
+ {
+    char str[10];
+    sprintf(str,"color %x",rand() % 16);
+    system(str);
+    sleep(0.5);
+ }
+    return 0;
+}
